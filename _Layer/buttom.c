093f3a0e7ba0c1d8e94f113fdf017e2821e5f4bd
jim Raynor <pch19980807@gmail.com>
2019-02-27 17:36:57 +0800

    BUG修复

--- _Layer/buttom.c
+++ _Layer/buttom.c
@@ -62,6 +62,7 @@ void Adc_Init(void)
   /* Enable ADC1, GPIOB and GPIOA clocks ********************************/  
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
