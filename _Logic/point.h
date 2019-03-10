9b472e49b9077c7f5b6ba7640ee12937ecbc973a
light <pch19980807@gamil.com>
2019-03-10 11:26:22 +0800

    添加了点逻辑文件
    添加了点逻辑文件

---
+++ _Logic/point.h
@@ -0,0 +1,15 @@
+#include "laser.h"
+#include "buttom.h"
+#include "math.h"
+/**
+  ******************************************************************************
+  * @file    Project/STM32F4xx_StdPeriph_Templates/line.h 
+  * @author  Debug406 Team
+  * @date    10-March-2019
+  * @brief   Header for main.c module
+  ******************************************************************************
+  * @attention
+	*	This file is used to describe the functions called when 
+	* hunting and how the related algorithms work.
+  ******************************************************************************
+  */
