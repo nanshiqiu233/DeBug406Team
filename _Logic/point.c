095b60df428266c4dda4303e17254d927d5b0d60
light <pch19980807@gamil.com>
2019-03-12 22:39:38 +0800

    点检测停止测试封装
    修复了上个版本中changed导致的bug，并封装一个简易的测试函数

--- _Logic/point.c
+++ _Logic/point.c
@@ -8,14 +8,16 @@ void _FindPoint()
 	{
 		temp+=adcData->array[0][i];
 	}
-	if(temp<=20114&&(IsLLaserChange() == Changed||IsRLaserChange() == UnChange))
+	if(temp<=20114&&(IsLLaserChange() == Changed||IsRLaserChange() == Changed))
 	{
-		UpdateMotorState(MOTOR_STOP);
+		UpdateMotorState(MOTOR_FRONT);
+		SetWheelSpeed(0.5,0.5);
+		SysTickDelay(100);
 	}
 	/*test only*/
 	//adcData->array[0][i];
-	printf("sum=%d\r\n",temp);
-	printf("%d\r\n",IsLLaserChange());
-	printf("%d\r\n",IsRLaserChange());
+//	printf("sum=%d\r\n",temp);
+//	printf("%d\r\n",IsLLaserChange());
+//	printf("%d\r\n",IsRLaserChange());
 	/*test only*/
 }
