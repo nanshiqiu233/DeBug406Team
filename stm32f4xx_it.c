753f8bf03b565e0db17e27e972e52e3872793a98
techguy10k <47449920+techguy10k@users.noreply.github.com>
2019-03-25 13:28:27 +0800

    ok

--- stm32f4xx_it.c
+++ stm32f4xx_it.c
@@ -203,6 +203,7 @@ void DMA1_Stream5_IRQHandler(void)
   */
 void DMA2_Stream5_IRQHandler(void)
 {
+	Command_Usart_Rx_Interupt();
 	DMA_ClearFlag(DMA2_Stream5,DMA_FLAG_TCIF5);
 }
 
@@ -213,6 +214,7 @@ void DMA2_Stream5_IRQHandler(void)
   */
 void DMA1_Stream1_IRQHandler(void)
 {
+	Command_Usart_Rx_Interupt();
 	DMA_ClearFlag(DMA1_Stream1,DMA_FLAG_TCIF5);
 }
 
